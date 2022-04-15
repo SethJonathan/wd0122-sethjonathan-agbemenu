004a648d640dac984c8e852a0063b2e56489e809
SethJonathan <jobest94@gmail.com>
2022-04-15 17:00:11 +0200

    compito

---
+++ m1/w3/d5/test settimanale/calolatrice/assets/js/script.js
@@ -0,0 +1,32 @@
+function aggiungiNumero(bottone){
+    let numero =bottone.innerHTML;
+    let display = document.querySelector('#display');
+    display.value += numero
+}
+
+function risultato(){
+    let display = document.querySelector('#display')
+    let totale = eval(display.value)
+    display.value=totale
+}
+
+function elimina(){
+    let display = document.querySelector('#display')
+    display.value = display.value.slice(0, -1);
+}
+
+function pulisci(){
+    let display = document.querySelector('#display')
+    display.value ='';
+}
+
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
