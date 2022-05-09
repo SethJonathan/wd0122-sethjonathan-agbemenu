5b4312b91c35647b44d60b52ce26b37d057e8e00
SethJonathan <jobest94@gmail.com>
2022-05-09 17:55:26 +0200

    esercizio

---
+++ m2/w3/d1/Esercizio Giornaliero/corretto/script.js
@@ -0,0 +1,8 @@
+function aggiungi(){
+    let n = document.querySelector("#nome").value
+    let c = document.querySelector("#cognome").value
+    let dn = document.querySelector("#nascita").value
+
+
+
+}
